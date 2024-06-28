e83b34687136a9bcee5d046bca665b6a6df859c2
fsanntiago <zerofabricio@hotmail.com>
2024-06-28 10:24:50 -0300

    feat: add EditAnswer use case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,6 +1,7 @@
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
+  save(answer: Answer): Promise<void>
   findById(id: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
   delete(answer: Answer): Promise<void>
