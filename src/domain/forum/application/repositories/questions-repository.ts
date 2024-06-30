e36a5f0fbb6ef45461d38f947ad9a48653223071
fsanntiago <zerofabricio@hotmail.com>
2024-06-30 11:09:25 -0300

    feat: add FetchRecentQuestions use case

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,6 +1,8 @@
+import { PaginationParams } from '@/shared/repositories/pagination-params'
 import { Question } from '../../enterprise/entities/question'
 
 export interface QuestionsRepository {
+  findManyRecent(params: PaginationParams): Promise<Question[]>
   save(question: Question): Promise<void>
   findBySlug(slug: string): Promise<Question | null>
   findById(id: string): Promise<Question | null>
