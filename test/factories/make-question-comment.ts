da8b36e1a0779832fbb62956f95d25b598455932
fsanntiago <zerofabricio@hotmail.com>
2024-07-01 14:21:25 -0300

    feat: add DeleteQuestionComment use case

---
+++ test/factories/make-question-comment.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityID } from '@/shared/entities/unique-entity-id'
+import {
+  QuestionComment,
+  QuestionCommentProps,
+} from '@/domain/forum/enterprise/entities/question-comment'
+
+export function makeQuestionComment(
+  override: Partial<QuestionCommentProps> = {},
+  id?: UniqueEntityID,
+) {
+  const questionComment = QuestionComment.create(
+    {
+      questionId: new UniqueEntityID(),
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
+
+  return questionComment
+}
