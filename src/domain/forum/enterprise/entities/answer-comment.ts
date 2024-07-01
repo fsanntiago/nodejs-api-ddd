3ba284afa6628d121c1046f438107e38478750c8
fsanntiago <zerofabricio@hotmail.com>
2024-07-01 11:46:05 -0300

    feat: add CommentOnQuestion and CommentOnAnswer use case

---
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -0,0 +1,28 @@
+import { UniqueEntityID } from '@/shared/entities/unique-entity-id'
+import { Optional } from '@/shared/types/optional'
+import { Comment, CommentProps } from './comment'
+
+export interface AnswerCommentProps extends CommentProps {
+  answerId: UniqueEntityID
+}
+
+export class AnswerComment extends Comment<AnswerCommentProps> {
+  get answerId() {
+    return this.props.answerId
+  }
+
+  static create(
+    props: Optional<AnswerCommentProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const answerComment = new AnswerComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return answerComment
+  }
+}
