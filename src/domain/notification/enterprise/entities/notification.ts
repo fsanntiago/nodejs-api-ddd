7d26eafed07809bd7ee495352416f541ce11c354
fsanntiago <zerofabricio@hotmail.com>
2024-07-05 15:46:30 -0300

    feat: add Send-Notification useCase

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,48 @@
+import { Entity } from '@/shared/entities/entity'
+import { UniqueEntityID } from '@/shared/entities/unique-entity-id'
+import { Optional } from '@/shared/types/optional'
+
+export interface NotificationProps {
+  recipientId: UniqueEntityID
+  title: string
+  content: string
+  createdAt: Date
+  readAt?: Date
+}
+
+export class Notification extends Entity<NotificationProps> {
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get readAt() {
+    return this.props.readAt
+  }
+
+  static create(
+    props: Optional<NotificationProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const notification = new Notification(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return notification
+  }
+}
